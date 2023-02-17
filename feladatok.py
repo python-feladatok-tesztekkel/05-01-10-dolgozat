cac068c46ea3c9ffde65bd9097551b2ef4376b8a
Gyuris Csaba <gycsaba72@gmail.com>
2023-02-17 08:47:13 +0100

    Update feladatok.py

--- feladatok.py
+++ feladatok.py
@@ -1,18 +1,19 @@
 # feladat.py
 
 # 1. feladat
-# Készítsen függvényt szog néven!
+# Készítsen függvényt "szog" néven!
 # A függvény bemeneti paramétere legyen egy szög mérete fokban megadva (egész szám)
-# A függvény a nullszög, hegyesszög, derékszög, tompaszög, egyenesszog, homorúszög, teljesszög szöveges értéeklésekkel térjen vissza (szöveg)
+# A függvény a nullszög, hegyesszög, derékszög, tompaszög, egyenesszög, homorúszög, teljesszög szöveges értéeklésekkel térjen vissza (szöveg)
 # 360 foknál nagyobb vagy nullánál kisebb szög esetén térjen vissza None értékkel
 
 # 2. feladat
-# Írjon függvényt hany_targybol_jeles néven
+# Írjon függvényt "hany_targybol_jeles" néven
 # A függvény bemenete egy lista, amelyben a diák féléves jegyei vannak felsorolva
 # Határozza meg, hogy hány tárgyból  jeles a diák
 # Feltételezheti, hogy a listában csak jegyek vannak, van jegy a listába, és hibás adat nincs benne
 
 
 # 1. feladat
-# Írjon függvényt ebetuk_szama néven
+# Írjon függvényt "ebetuk_szama" néven
 # A függvény határozza meg a paraméterben kapott stringben hány "e" és "é" betű van!
+# A függvény bemenete csak egy string
